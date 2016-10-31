44681826c77cf20a2d78897910edadc37b18a2b3
shyamshankarhr <shyamshankarcern@gmail.com>
2016-10-31 06:27:47 +0530

    Made splashscreen (unanimated)

---
+++ app/src/main/java/shaastra/com/android_app_2017/Splashscreen.java
@@ -0,0 +1,39 @@
+package shaastra.com.android_app_2017;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+/**
+ * Created by Shyam Shankar on 31-10-2016.
+ */
+public class Splashscreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(Splashscreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
